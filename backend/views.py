63de0f3357f6f69c02a6d7d202998122045c499f
islam <nudinovislam4@gmail.com>
2022-02-04 17:12:48 +0600

    документация

--- backend/views.py
+++ backend/views.py
@@ -3,18 +3,19 @@ import datetime
 
 from backend.forms import DateForm
 
+"""Приедстовления для определения номера недели"""
+
 
 def response_date(request, *args, **kwargs):
-    if request.method == 'GET':
+    if request.method == 'GET':  # если запрос гет до топравь форму
         form = DateForm()
         return render(request, 'index.html', context={'form': form})
-    elif request.method == 'POST':
-        form = DateForm(data=request.POST)
-        if form.is_valid():
-            response = form.cleaned_data['date'].isocalendar()[1]
+    elif request.method == 'POST':  # если запрос POST
+        form = DateForm(data=request.POST)  # получаем data из requesta
+        if form.is_valid():  # проверка на волидность
+            response = form.cleaned_data['date'].isocalendar()[1]  # определяем номер недели
             context = {
                 'form': form,
                 'week_number': response
             }
-            return render(request, 'index.html', context)
-
+            return render(request, 'index.html', context)  # Вернули результат
